f77a0e897a924c59e36a52fcb5a5042145dd0ff1
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-07 10:41:57 +0530

    copy constructor

---
+++ problem_30.cpp
@@ -0,0 +1,23 @@
+//copy constractor
+#include <iostream>
+using namespace std;
+class student{
+    int age;
+    public:
+    student  (int a){
+        age = a;
+    }
+    student(student &s){
+        age=s.age;
+    }
+    void display(){
+        cout<<"the age is"<<age<<endl;
+    }
+};
+int main (){
+    student muskan(19);
+    student utkarsh=muskan;
+    muskan.display();
+    utkarsh.display();
+
+}
