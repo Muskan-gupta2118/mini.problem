d4613919cfacb29df9a12be8d7bc4a4a36ac0512
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-08 20:19:50 +0530

    private member(error)

---
+++ problem_33.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class student{
+    private:
+    //char name[20]:
+    //int age;
+    void input(){
+        cout<<"Enter name : "<<endl;
+      //  cin>>name;
+    }
+};
+int main(){
+    student s1;
+    //s1.input();
+    return 0;
+}
