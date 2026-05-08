3d58e3c0aedd4669712d03decc7b3c8758531869
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-08 20:33:08 +0530

    accessing private member

---
+++ problem_34.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ 
+class student{
+    private:
+    char name[20];
+    int age;
+    public:
+    void input(){
+        cout<<"Enter name :"<<endl;
+        cin>>name;
+        cout<<"Enter age :";
+        cin>>age;
+    }
+    void display(){
+        cout<<"the name of student is :"<<name<<endl;
+        cout<<"the age is : "<<age;
+    }
+};
+int main(){
+    student muskan;
+    muskan.input();
+    muskan.display();
+    return 0;
+}
