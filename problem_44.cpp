da07988942c0c51fe4b8a06bcc606fbc3d1adbcc
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-10 15:56:26 +0530

    inheritance like hybrid

---
+++ problem_44.cpp
@@ -0,0 +1,41 @@
+//inheritance likhe hybrid(important)
+#include <iostream>
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    int id;
+
+public:
+    Employee(string n, int i) {
+        name = n;
+        id = i;
+    }
+};
+
+class Manager : public Employee {
+private:
+    int teamSize;
+
+public:
+    Manager(string n, int i, int t)
+        : Employee(n, i)
+    {
+        teamSize = t;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+        cout << "Team Size: " << teamSize << endl;
+    }
+};
+
+int main() {
+    Manager m1("Rahul", 201, 15);
+
+    m1.display();
+
+    return 0;
+}
