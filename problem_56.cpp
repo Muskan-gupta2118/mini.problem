b17cdb9afc48562578af994c85cc075f981055ce
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-13 22:15:22 +0530

    constructor and deconstructor in inheritance

---
+++ problem_56.cpp
@@ -0,0 +1,23 @@
+//constructor and deconstructor in inheritance
+#include<iostream>
+using namespace std;
+class grandparent{
+    public:
+    grandparent(){
+        cout<<"you can live in my house"<<endl;
+
+    }
+};
+class parent:public grandparent{
+    public:
+    parent(){
+        cout<<"parent class"<<endl;
+    }
+};
+int main(){
+    grandparent gp;
+    parent p1;
+}
+
+
+
