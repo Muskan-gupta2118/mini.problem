67e969b08c9d31bc106d5850cf34618f4dde9f82
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-14 22:56:13 +0530

    employee salary system

---
+++ problem_58.cpp
@@ -0,0 +1,41 @@
+//employee salary system
+#include <iostream>
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    int basicSalary;
+
+public:
+    Employee(string n, int s) {
+        name = n;
+        basicSalary = s;
+    }
+};
+
+class Manager : public Employee {
+private:
+    int bonus;
+
+public:
+    Manager(string n, int s, int b)
+        : Employee(n, s)
+    {
+        bonus = b;
+    }
+
+    void totalSalary() {
+        cout << "Manager Name: " << name << endl;
+        cout << "Total Salary: "
+             << basicSalary + bonus << endl;
+    }
+};
+
+int main() {
+    Manager m1("Rahul", 50000, 10000);
+
+    m1.totalSalary();
+
+    return 0;
+}
