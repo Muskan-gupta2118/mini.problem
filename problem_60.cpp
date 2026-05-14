b9baa5a8efd37a8f86b6596503ab1f402e69189e
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-14 22:59:18 +0530

    bank account system

---
+++ problem_60.cpp
@@ -0,0 +1,35 @@
+//bank account system
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+protected:
+    int balance;
+
+public:
+    BankAccount() {
+        balance = 1000;
+    }
+};
+
+class SavingsAccount : public BankAccount {
+public:
+    void deposit(int amount) {
+        balance += amount;
+    }
+
+    void display() {
+        cout << "Balance: "
+             << balance << endl;
+    }
+};
+
+int main() {
+    SavingsAccount s;
+
+    s.deposit(500);
+
+    s.display();
+
+    return 0;
+}
