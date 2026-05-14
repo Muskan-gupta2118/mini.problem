e73597e818c494c829598def57a2b038691d6b78
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-14 22:58:46 +0530

    student result system

---
+++ problem_59.cpp
@@ -0,0 +1,38 @@
+//student result management
+#include <iostream>
+using namespace std;
+
+class Student {
+protected:
+    int marks1, marks2;
+
+public:
+    void getMarks(int m1, int m2) {
+        marks1 = m1;
+        marks2 = m2;
+    }
+};
+
+class Result : public Student {
+public:
+    void displayResult() {
+        int total = marks1 + marks2;
+
+        cout << "Total Marks: " << total << endl;
+
+        if(total >= 80)
+            cout << "Pass" << endl;
+        else
+            cout << "Fail" << endl;
+    }
+};
+
+int main() {
+    Result r;
+
+    r.getMarks(40, 50);
+
+    r.displayResult();
+
+    return 0;
+}
