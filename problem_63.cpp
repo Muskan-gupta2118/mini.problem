62a115f57fb6fbc3c14ddfdb05c30e6c56fbdb15
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-15 22:10:05 +0530

    private member

---
+++ problem_63.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Parent {
+public:
+    int x = 20;
+};
+
+class Child : private Parent {
+public:
+    void display() {
+        cout << x;
+    }
+};
+
+int main() {
+    Child c;
+
+    c.display();
+
+    return 0;
+}
