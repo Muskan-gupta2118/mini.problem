95467d808a7c1953518d25cfe15694a425c631fc
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-18 18:33:31 +0530

    fixing method hiding

---
+++ problem_76.cpp
@@ -0,0 +1,27 @@
+//fixing method hiding
+#include <iostream>
+using namespace std;
+
+class Parent {
+public:
+    void show() {
+        cout << "Parent Show" << endl;
+    }
+};
+
+class Child : public Parent {
+public:
+    void show(int x) {
+        cout << "Child Show" << endl;
+    }
+};
+
+int main() {
+    Child c;
+
+    // c.show(); ❌ ERROR
+
+    c.show(10);
+
+    return 0;
+}
