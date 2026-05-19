40a5d913e9407612ee98ba2b21b20880c51af291
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-19 22:48:36 +0530

    online shop cart

---
+++ problem_78.cpp
@@ -0,0 +1,36 @@
+//online shop cart
+#include <iostream>
+using namespace std;
+
+class Laptop {
+private:
+    string brand;
+    int price;
+
+public:
+    Laptop() {
+        brand = "HP";
+        price = 40000;
+    }
+
+    Laptop(string b, int p) {
+        brand = b;
+        price = p;
+    }
+
+    void display() {
+        cout << brand
+             << " "
+             << price << endl;
+    }
+};
+
+int main() {
+    Laptop l1;
+    Laptop l2("Dell", 60000);
+
+    l1.display();
+    l2.display();
+
+    return 0;
+}
