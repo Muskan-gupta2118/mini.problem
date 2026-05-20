dec4cbe6f58abee7851d4fad0b4235972b97960c
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-20 20:25:49 +0530

    basic virtual function

---
+++ problem_84.cpp
@@ -0,0 +1,29 @@
+//basic virtual function 
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal makes sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Animal* ptr;
+
+    Dog d;
+
+    ptr = &d;
+
+    ptr->sound();
+
+    return 0;
+}
