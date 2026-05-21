35210c35f142b57ce2aa97ccbbd84d1561ee6801
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-21 23:17:37 +0530

    basic freind function

---
+++ problem_87.cpp
@@ -0,0 +1,28 @@
+//basic freind function
+#include <iostream>
+using namespace std;
+
+class Box {
+private:
+    int width;
+
+public:
+    Box() {
+        width = 10;
+    }
+
+    friend void show(Box b);
+};
+
+void show(Box b) {
+    cout << "Width: "
+         << b.width << endl;
+}
+
+int main() {
+    Box b;
+
+    show(b);
+
+    return 0;
+}
