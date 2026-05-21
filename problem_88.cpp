b82248429c6821ac793a08bc2b931100e4f204e4
Muskan Gupta <muskangupta2118@gmail.com>
2026-05-21 23:18:13 +0530

    maximum from freind function

---
+++ problem_88.cpp
@@ -0,0 +1,34 @@
+//maximum using freind function
+#include <iostream>
+using namespace std;
+
+class Number {
+private:
+    int a, b;
+
+public:
+    Number(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    friend void maximum(Number n);
+};
+
+void maximum(Number n) {
+    if(n.a > n.b)
+        cout << "Maximum: "
+             << n.a << endl;
+
+    else
+        cout << "Maximum: "
+             << n.b << endl;
+}
+
+int main() {
+    Number n(10, 20);
+
+    maximum(n);
+
+    return 0;
+}
