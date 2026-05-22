21b5ff57984feb770602f6a239bf32c37c415f88
Muskan gupta <muskangupta2118@gmail.com>
2026-05-22 21:22:47 +0530

    calculator using friend function

---
+++ problem_92.cpp
@@ -0,0 +1,28 @@
+//calculator using friend function
+#include <iostream>
+using namespace std;
+
+class Calculator {
+private:
+    int num;
+
+public:
+    Calculator(int n) {
+        num = n;
+    }
+
+    friend void square(Calculator c);
+};
+
+void square(Calculator c) {
+    cout << "Square = "
+         << c.num * c.num << endl;
+}
+
+int main() {
+    Calculator c(5);
+
+    square(c);
+
+    return 0;
+}
