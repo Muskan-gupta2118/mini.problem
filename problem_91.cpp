d497fa10a4d3381a788077d2ad22a1811667177b
Muskan gupta <muskangupta2118@gmail.com>
2026-05-22 21:21:51 +0530

    swap using friend function

---
+++ problem_91.cpp
@@ -0,0 +1,40 @@
+//swap using friend function
+#include <iostream>
+using namespace std;
+
+class Swap {
+private:
+    int a, b;
+
+public:
+    Swap(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    friend void exchange(Swap &s);
+};
+
+void exchange(Swap &s) {
+    int temp;
+
+    temp = s.a;
+    s.a = s.b;
+    s.b = temp;
+
+    cout << "After Swapping:" << endl;
+
+    cout << "a = "
+         << s.a << endl;
+
+    cout << "b = "
+         << s.b << endl;
+}
+
+int main() {
+    Swap s(10, 20);
+
+    exchange(s);
+
+    return 0;
+}
