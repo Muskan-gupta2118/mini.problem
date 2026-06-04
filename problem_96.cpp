26ac9f90bf94ad86423f9d56df3769b8ee675c88
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-04 10:51:08 +0530

    sturcture with address

---
+++ problem_96.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct student{
+    string name;
+    int rollno;
+    int marks;
+
+
+};
+int main(){
+    student s;
+    s.name="Muskan";
+    s.rollno=138;
+    s.marks=500;
+    cout<<"the name of student is "<<s.name<<endl;
+    cout<<&s.name<<endl;
+    cout<<"the roll no. of sudent is "<<s.rollno<<endl<<endl;
+    cout<<&s.rollno<<endl;
+    cout<<"the marks of student is "<<s.marks<<endl;
+    cout<<&s.marks<<endl;
+return 0;
+}
