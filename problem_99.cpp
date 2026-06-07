fd529bf3bebd0088fd7b2af80a02c4aa03f96c19
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-07 22:00:56 +0530

    read data from a file

---
+++ problem_99.cpp
@@ -0,0 +1,16 @@
+//read data from a file
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream file("data.txt");
+    string line;
+
+    while(getline(file, line)) {
+        cout << line << endl;
+    }
+
+    file.close();
+    return 0;
+}
