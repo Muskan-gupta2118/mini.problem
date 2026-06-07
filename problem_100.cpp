aa8bb16716ecafc96a3b3d1b0e7aa9b696cc4305
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-07 22:01:18 +0530

    append data to a existing file

---
+++ problem_100.cpp
@@ -0,0 +1,16 @@
+//append data to a existing file
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream file("data.txt");
+    string line;
+
+    while(getline(file, line)) {
+        cout << line << endl;
+    }
+
+    file.close();
+    return 0;
+}
