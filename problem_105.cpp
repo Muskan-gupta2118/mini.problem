953bac368f0871a6403801695b3c9b679fef7572
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-08 23:29:33 +0530

    heirachical  inheritance

---
+++ problem_105.cpp
@@ -0,0 +1,23 @@
+//heirachical inheritance
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    void show() {
+        cout << "Shape Class" << endl;
+    }
+};
+
+class Rectangle : public Shape {};
+class Circle : public Shape {};
+
+int main() {
+    Rectangle r;
+    Circle c;
+
+    r.show();
+    c.show();
+
+    return 0;
+}
