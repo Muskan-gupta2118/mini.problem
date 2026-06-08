e20af02ca8de029a1d95c813dc3847231cb3a847
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-08 23:27:58 +0530

    single inheritance

---
+++ problem_102.cpp
@@ -0,0 +1,24 @@
+//single inheritance
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+    void displayPerson() {
+        cout << "Person Details" << endl;
+    }
+};
+
+class Student : public Person {
+public:
+    void displayStudent() {
+        cout << "Student Details" << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.displayPerson();
+    s.displayStudent();
+    return 0;
+}
