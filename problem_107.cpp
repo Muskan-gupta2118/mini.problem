45a8c06d9d44fe32dbbc8331c201c284b0275c5d
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-09 21:55:48 +0530

    write file

---
+++ problem_107.cpp
@@ -0,0 +1,11 @@
+//file handling
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+ofstream file("new.txt");
+file<<"hello world";
+file.close();
+cout<<"Data written successfully";
+return 0;
+}
