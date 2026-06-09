5b97f05e0aa9ecadd9e70249cd4a1496d93ab8e7
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-09 21:56:09 +0530

    read file

---
+++ problem_108.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+ ifstream file("new.txt");
+ string line;
+ while(getline(file,line)){
+    cout<<line<<endl;
+ }
+ file.close();
+ return 0;
+}
