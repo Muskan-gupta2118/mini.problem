a7fdfa4d5bc5cb5f63e7a97e6f60bc69094247ec
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-16 22:26:47 +0530

    heirachical inheritance

--- problem_109.cpp
+++ problem_109.cpp
@@ -5,6 +5,6 @@ int main(){
     ofstream file("new.txt",ios::app);
     file<<"\nNew data";
     file.close();
-    cout<<" ok adding successfully";
+    cout<<" ok adding  successfully";
     return 0;
 }
